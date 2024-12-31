a35af6919e435a2eb806377acf8b9ba05c91c965
Das_M_e_e_ <151558417+Das-M-e-e@users.noreply.github.com>
2024-12-31 19:09:58 +0100

    assembly infos

--- tschiraplusAPI/obj/Debug/net9.0/tschiraplusAPI.AssemblyInfo.cs
+++ tschiraplusAPI/obj/Debug/net9.0/tschiraplusAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tschiraplusAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f64a81e7b8916b4bc8b95cef2a1a20e72f1963f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12f104d1506518e2eb1e8c6dc74086a9c749260")]
 [assembly: System.Reflection.AssemblyProductAttribute("tschiraplusAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tschiraplusAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
